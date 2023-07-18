44bf1ee47db1b4a9df2268ec53ae4273033671dc
Connor McGrew <connormcgrew003@gmail.com>
2023-07-17 22:58:22 -0600

    initial commit

---
+++ react-portfolio/pages/index.js
@@ -0,0 +1,16 @@
+import head from 'next/head';
+
+export default function Home() {
+    return (
+        <div>
+            <head>
+                <title>Connor McGrew Portfolio</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </head>
+
+            <main>
+                <h1>Home Page</h1>
+            </main>
+        </div>
+    );
+}
