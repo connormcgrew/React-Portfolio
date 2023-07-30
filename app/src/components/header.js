28f89c938f2ef2f886258ff004c7d96b47f9cf40
Connor McGrew <connormcgrew003@gmail.com>
2023-07-30 04:36:49 -0600

    changed header color gradiant direction

--- app/src/components/header.js
+++ app/src/components/header.js
@@ -7,7 +7,7 @@ const Header = () => {
   const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
 
   return (
-    <nav className={`py-10 flex justify-between ${darkMode ? 'bg-gradient-to-r from-white to-black text-white' : 'bg-gradient-to-r from-black to-white'}`}>
+    <nav className={`py-10 flex justify-between ${darkMode ? 'bg-gradient-to-r from-black to-white text-white' : 'bg-gradient-to-r from-white to-black'}`}>
       <NavLink to="/">
         <h1 className={`text-xl ${darkMode ? 'bg-neutral-900 text-zinc-200'  : 'bg-zinc-200 text-black'} hover:bg-pink-500 hover:text-white focus:bg-pink-500 focus:text-white transition-colors duration-300 ease-in-out px-4 py-2 rounded-md ml-8`}>Connor McGrew</h1>
       </NavLink>
--- app/src/pages/home.js
+++ app/src/pages/home.js
@@ -9,13 +9,18 @@ export default function Home() {
         <section className='min-h-screen'>
 
           <div className='relative mx-auto overflow-hiddens pt-10 flex justify-center'>
-            <img src="/images/headshotphotoshop.png" alt="Picture of the author" className='rounded-full w-80 h-80 object-cover'/>
+            <img src="/images/headshotphotoshop.png" alt="Author's Headshot" className='rounded-full w-80 h-80 object-cover'/>
           </div>
           <div className='text-center p-10'>
-            <h2 className='text-5xl  mb-4 py-2'>Connor McGrew</h2>
+            <h2 className='text-6xl  mb-4 py-2'>Connor McGrew</h2>
             <h3 className='text-2xl py-2'>Fullstack Developer and Student</h3>
             <p className='text-md py-5 leading-8'>
-              I am currently a student at the University of Utah Professional Education Bootcamps studying Full-Stack Development. I am located in Lehi and have around 6 months of experience. I am currently looking for a job in the field of Full-Stack Development, Front-End Developer, or Back-End Developer. I am a hard worker and a quick learner. I am also a very good team player and I am very easy to get along with. I am very passionate about coding and I am very excited to start my career!
+              I am currently a student at the University of Utah Professional Education Bootcamps studying Full-Stack Development.
+               I am located in Lehi and have 6 months of experience. 
+               I am currently looking for a job in the field of Full-Stack Development, Front-End Developer, or Back-End Developer. 
+               I am a hard worker and a quick learner.
+                I am also a very good team player and I am very easy to get along with.
+                 I am very passionate about coding and I am very excited to start my career!
             </p>
           </div>
         </section>
