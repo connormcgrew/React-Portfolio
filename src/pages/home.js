160175c57d8f186bf5ef05b57e7d9470724bbf0c
Connor McGrew <connormcgrew003@gmail.com>
2023-08-12 19:58:36 -0600

    updated home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -13,12 +13,12 @@ export default function Home() {
           </div>
           <div className='text-center p-10'>
             <h2 className='text-6xl  mb-4 py-2'>Connor McGrew</h2>
-            <h3 className='text-2xl py-2'>Fullstack Developer and avid learner</h3>
+            <h3 className='text-2xl py-2'>Fullstack Developer</h3>
             <p className='text-md py-5 leading-8'>
               I am a recent grauduate of the University of Utah Professional Education Bootcamps studying Full-Stack Development.
                I am located in Lehi and have 6 months of coding experience. 
                I am currently looking for a job in the field of Full-Stack Development, Front-End Developer, or Back-End Developer. 
-               I am a hard worker and a quick learner.
+               I am a hard worker and an avid learner.
                 I am also a very good team player and I am very easy to get along with.
                  I am very passionate about coding and I am very excited to start my career!
             </p>
